4e36d9184d4de3979211b380c69b067bedc3f4fb
HakujouRyu <47827212+HakujouRyu@users.noreply.github.com>
2019-10-29 16:30:16 -0400

    Conform to PEP8

--- lambdata_hakujouryu/df_utils.py
+++ lambdata_hakujouryu/df_utils.py
@@ -1,4 +1,4 @@
-""" 
+"""
 utilitiy funcs for working with DataFrames
 """
 
@@ -7,11 +7,12 @@ import numpy as np
 
 
 TEST_DF = pd.DataFrame(
-    {'one': [1,1,1,1], 'two': [2,2,2,2],
+    {'one': [1, 1, 1, 1], 'two': [2, 2, 2, 2],
      'dates': ['09/09/1988', '04/02/1992', '01/10/2009', '04/20/1920']})
 
 
-TEST_LIST = [9,9,9,9]
+TEST_LIST = [9, 9, 9, 9]
+
 
 class DateFunctions:
     """ Contains useful functions for manipulating Dates """
@@ -19,26 +20,26 @@ class DateFunctions:
         pass
 
     def split_dates(self, df, column):
-        """ Converts ['created_at', 'dealdine', 'launched_at'] into pandas.dt, 
+        """ Converts ['created_at', 'dealdine', 'launched_at'] into pandas.dt,
             and creates columns for month day and year.
             Also creates 'days_to_launch' and 'campaign_length' columns """
-
         df[column] = pd.to_datetime(df[column])
 
-        #Break time up into columns Month day etc
+#        Break time up into columns Month day etc
         df[f'{column}_month'] = df[column].dt.month
         df[f'{column}_dow'] = df[column].dt.weekday
         df[f'{column}_year'] = df[column].dt.year
         return df
 
-
     def time_to_string(self, df, column):
-        """  Convert back into strings so that we can pass to model """    
+        """  Convert back into strings so that we can pass to model """
         df[column] = df[column].astype(str)
         return df
 
+
 def list_to_col(ls, df, list_name):
-    """ Converts ls (list) to a Series then joins to df as column 'list_name' """
+    """ Converts ls (list) to a Series then joins
+         to df as column 'list_name' """
 
     df[list_name] = pd.Series(ls)
     return df
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
     setuptools.setup(
     name="lambdata-hakujouryu",
-    version = "0.1.2",
+    version = "0.1.22",
     author = "HakujouRyu",
     description = "a collection of data science helper functions",
     long_description = LONG_DESCRIPTION,
